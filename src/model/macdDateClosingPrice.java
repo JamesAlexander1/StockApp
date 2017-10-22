5e334f682dfd8face52a50448ac34d3acec23301
JamesAlexander1 <jamesbrooksalexander@gmail.com>
2017-10-22 17:29:16 +1100

    fixed stuff again

---
+++ src/model/macdDateClosingPrice.java
@@ -0,0 +1,71 @@
+package model;
+
+
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+
+public class macdDateClosingPrice implements DatePricePoint<macdDateClosingPrice>{
+
+    private GregorianCalendar shortDate;
+    private Double macdPrice;
+    private Double macdHistPrice;
+    private Double macdSignalPrice;
+    
+    
+    public macdDateClosingPrice(GregorianCalendar newDate, Double macd, Double macdHist, Double macdSignal){
+        
+        shortDate = newDate;
+        macdPrice = macd;
+        macdHistPrice = macdHist;
+        macdSignalPrice = macdSignal;
+        
+    }
+    public GregorianCalendar getDate() {
+       
+        return shortDate;
+    }
+
+    public void setDate(GregorianCalendar newDate) {
+        shortDate = newDate;
+
+    }
+
+    public Double getMacdPrice() {
+        return macdPrice;
+    }
+    
+    public Double getMacdHistPrice() {
+        return macdHistPrice;
+    }
+    
+    public Double getMacdSignalPrice() {
+        return macdSignalPrice;
+    }
+    
+    public Integer getYear(){
+        return shortDate.get(Calendar.YEAR);
+    }
+    
+    public Integer getMonth(){
+        return shortDate.get(Calendar.MONTH);
+    }
+    
+    public Integer getDay(){
+        return shortDate.get(Calendar.DAY_OF_MONTH);
+    }
+    @Override
+    public int compareTo(macdDateClosingPrice o) {
+        
+        return o.getDate().get(Calendar.DAY_OF_MONTH) - this.shortDate.get(Calendar.DAY_OF_MONTH);
+    }
+    @Override
+    public Double getPrice() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    @Override
+    public void setPrice(Double newPrice) {
+        // TODO Auto-generated method stub
+        
+    }
+}
